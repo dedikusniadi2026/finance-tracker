ca3bc22e0829c798eea148c17172dcef470f6387
dedikusniadi2026 <dedikusniadi.bisnis@gmail.com>
2024-12-09 14:09:11 +0700

    bug

---
+++ .nuxt/types/supabase.d.ts
@@ -0,0 +1,6 @@
+declare module '#supabase/server' {
+  const serverSupabaseClient: typeof import('/home/dedikusniadi/Documents/nuxt/fin/finance-tracker/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseClient
+  const serverSupabaseServiceRole: typeof import('/home/dedikusniadi/Documents/nuxt/fin/finance-tracker/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseServiceRole
+  const serverSupabaseUser: typeof import('/home/dedikusniadi/Documents/nuxt/fin/finance-tracker/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseUser
+  const serverSupabaseSession: typeof import('/home/dedikusniadi/Documents/nuxt/fin/finance-tracker/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseSession
+}
